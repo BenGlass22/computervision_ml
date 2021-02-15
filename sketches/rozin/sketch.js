4c639d7c9b4e612ab7e804b234400203270dac22
Ben Glass <60358064+BenGlass22@users.noreply.github.com>
2021-02-15 16:52:23 -0500

    added bianry sketch and centered the sketch

--- sketches/rozin/sketch.js
+++ sketches/rozin/sketch.js
@@ -1,20 +1,29 @@
-let w = 320;
-let h = 180;
+//B3n Gl422
 
+let w = 640;
+let h = 360;
+//let w = 1280;
+//let h = 720;
 
+let fr= 24;
 
 function setup() {
-  createCanvas(w, h);
+  let canvas = createCanvas(w, h);
+  canvas.parent("#sketch-parent");
+   frameRate(fr);
   pixelDensity(1);
   capture = createCapture(VIDEO);
   capture.size(w, h);
   capture.hide();
-  //ellipseMode(CORNER);
+  capture.position(CENTER);
+  
+    
+  
 }
 
-function draw() {
-  
-  let stepSize = 10;
+  function draw() {
+  background(0,0,0,30);
+  let stepSize = 6;
   capture.loadPixels();
 
   
@@ -26,52 +35,38 @@ function draw() {
       let g = capture.pixels[index+1];
       let b = capture.pixels[index+2];
       
-      let c = color(r, g, b);
+      
       
       let totalBrightness = r + g + b;
       
-      let brightness = totalBrightness/2;
+      let brightness = totalBrightness/1.5;
       
-      var threshold = 200;
+      var threshold = 127.5;
       
       if(brightness > threshold ) {
-        fill(255);
-      } else{ fill(0);
+ 
+         //fill(255);
+        fill(57, 255, 20)
+        text('1', x, y);
+      } else{ 
+        text('0', x, y)
+        //fill(255);
+        fill(50);
         
       }
+        
       
-      //let size = map(brightness, 0, 255, stepSize/4, stepSize*2);
+  
+     map(brightness, 0, 255, stepSize/4, stepSize*2);
       
-      //fill(255, 200, 225);
-      //noStroke();
-      //noFill();
-      //fill(c);
-      
-      
-      // ellipse!
-      // push();
-      //   translate(width, 0);
-      //   scale(-1,1);
-      //     push();
-      //       translate(-stepSize/2, stepSize/2);
-      //   ellipse(x, y, size, size);
-      //   pop();
-      // pop();
-      
-        push();
-       translate(capture.width, 0);
-      
-        scale(-1,1);
-          push();
-            translate(-stepSize/2, stepSize/2);
-        rect(x, y, 5, 5);
-        pop();
-      pop();
+        
+          
+
       
    
     }
   }
   capture.updatePixels();
   
-  //image(capture, 0, 0);
+
 }
